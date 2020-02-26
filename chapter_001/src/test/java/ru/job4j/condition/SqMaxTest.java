dbaf8e3fdf449294e88adbc03f0b74a8cc60b7ba
Codonova <r.kosolapchenkov@gmail.com>
2020-02-26 09:50:09 +0300

    4.5. Отладка программы в IDEA[#235922] NEW

--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -6,10 +6,17 @@ import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
     @Test
-    public void errorFind() {
+    public void errorFindFirst() {
         SqMax check = new SqMax();
         int result = check.max(6, 4, 7,5);
         assertThat(result, is(7));
+    }
+
+    @Test
+    public void errorFindSecond() {
+        SqMax check = new SqMax();
+        int result = check.max(6, 8, 10,5);
+        assertThat(result, is(10));
     }
 
 }
