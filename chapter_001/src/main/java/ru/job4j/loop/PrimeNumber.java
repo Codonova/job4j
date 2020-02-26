2778d3ff49fab97d303d4b38751a8f45d37fbeed
Codonova <r.kosolapchenkov@gmail.com>
2020-02-26 10:21:11 +0300

    5.6. Простые числа.[#236001] Correct

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,14 +3,10 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        for (int i = 0; i <= finish; i++) {
+        for (int i = 2; i <= finish; i++) {
             if (CheckPrimeNumber.check(i)) {
                 count++;
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> fa378ef6ad68e7d6d771d73ddd98351aff658e25
         }
         return count;
     }
--- chapter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -2,13 +2,10 @@ package ru.job4j.loop;
 
 import org.junit.Test;
 
-<<<<<<< HEAD
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
-=======
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.is;
->>>>>>> fa378ef6ad68e7d6d771d73ddd98351aff658e25
+
 
 public class PrimeNumberTest {
     @Test
@@ -19,7 +16,6 @@ public class PrimeNumberTest {
     }
 
     @Test
-<<<<<<< HEAD
     public void when11() {
         PrimeNumber prime = new PrimeNumber();
         int count = prime.calc(11);
@@ -31,16 +27,5 @@ public class PrimeNumberTest {
         PrimeNumber prime = new PrimeNumber();
         int count = prime.calc(2);
         assertThat(count, is(1));
-=======
-    public void when4() {
-        boolean rsl = CheckPrimeNumber.check(4);
-        assertThat(rsl, is(false));
-    }
-
-    @Test
-    public void when1() {
-        boolean rsl = CheckPrimeNumber.check(1);
-        assertThat(rsl, is(false));
->>>>>>> fa378ef6ad68e7d6d771d73ddd98351aff658e25
     }
 }
