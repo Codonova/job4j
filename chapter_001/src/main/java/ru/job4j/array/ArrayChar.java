6a7797a06c30276c2df2dddbc1d54b45c9d03746
Codonova <r.kosolapchenkov@gmail.com>
2020-03-04 06:38:39 +0300

    6.4. Слово начинается с ...[#235933] NEW

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,10 @@ public class ArrayChar {
         boolean result = false;
 
         for (int i = 0; i < pref.length; i++) {
-            result = word[i] == pref[i];
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
+            } else result = true;
         }
         return result;
     }
