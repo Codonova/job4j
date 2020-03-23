6c5879e2155a58183b065b24972b40cee04bba64
Codonova <r.kosolapchenkov@gmail.com>
2020-03-23 16:59:29 +0300

    3. Перегрузить метод max для трех чисел[#236098]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -11,13 +11,37 @@ public class Max {
     /**
      * Поиск максимального значения из двух чисел
      *
-     * @param left  первое число
-     * @param right второе число
+     * @param first  первое число
+     * @param second второе число
      * @return result
      */
-    public static int max(int left, int right) {
-        boolean max = left > right;
-        int result = max ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        int max = first > second ? first : second;
+        return max;
+    }
+
+    /**
+     * Поиск максимального значения из трех чисел
+     * @param first
+     * @param second
+     * @param third
+     * @return
+     */
+
+    public static int max(int first, int second, int third) {
+         return max(first, max(second, third));
+    }
+
+    /**
+     * Поиск максимального значения из четырех чисел
+     * @param first
+     * @param second
+     * @param third
+     * @param forth
+     * @return
+     */
+
+    public static int max(int first, int second, int third, int forth) {
+        return max(first, max(second, third, forth));
     }
 }
