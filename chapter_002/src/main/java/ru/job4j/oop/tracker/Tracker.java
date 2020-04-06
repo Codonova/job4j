1b1bfd32565e9f3c3eaa76ce17ed68bb7445c4b6
Codonova <r.kosolapchenkov@gmail.com>
2020-04-06 14:55:18 +0300

    5. Реализовать класс Tracker[#236123]

---
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/Tracker.java
@@ -0,0 +1,92 @@
+package ru.job4j.oop.tracker;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод возвращает копию массива без пустых значений.
+     *
+     * @return массив без пустых ячеек
+     */
+
+    public Item[] findAll() {
+        Item[] WithOutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < position; i++) {
+            if (items[i] != null) {
+                WithOutNull[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(WithOutNull, size);
+
+    }
+
+    /**
+     * Метод ищет имена в массиве
+     * @param key
+     * @return Массив имен
+     */
+
+    public Item[] findByName(String key) {
+        Item[] names = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < position; i++) {
+            if (items[i].getName().equals(key)) {
+                names[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(names, size);
+    }
+
+    /**
+     * ПОиск по уникальному коду заявки
+     * @param id
+     * @return возращает уникальный код заявки, если не находит возвращает null.
+     */
+    public Item findById(String id) {
+        Item rsl = null;
+        for (int i = 0; i < position; i++) {
+            if (items[i].getId().equals(id)) {
+                rsl = items[i];
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод добавления заявки в хранилище
+     *
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/oop/tracker/TrackerTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.oop.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Solo");
+        Item item1 = new Item("Khan");
+        tracker.add(item);
+        tracker.add(item1);
+        Item[] result = tracker.findByName(item.getName());
+        assertThat(result, is(new Item[] {item}));
+    }
+
+    @Test
+    public void whenFindAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Koko");
+        Item item1 = new Item("Lala");
+        Item[] items = {item, item1};
+        tracker.add(item);
+        tracker.add(item1);
+        Item[] result = tracker.findAll();
+        assertThat(result, is(items));
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Koshka");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getId(), is(item.getId()));
+    }
+
+}
