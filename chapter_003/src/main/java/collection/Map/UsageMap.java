f74ccca11f2de3364ce7fefa52e94ffa8bd9edb8
Codonova <r.kosolapchenkov@gmail.com>
2020-06-08 13:38:02 +0300

    Map, HashMap[212652#236041]

---
+++ chapter_003/src/main/java/collection/Map/UsageMap.java
@@ -0,0 +1,14 @@
+package collection.Map;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("r.kosolapchenkov@ya.ru", "Ruslan Kosolapchenkov");
+        for(String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
