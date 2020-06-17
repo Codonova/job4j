e0e3269a55a28aacd40ba713e35a0f69c951411c
Codonova <r.kosolapchenkov@gmail.com>
2020-06-17 13:48:57 +0300

    Компаратор для строк.[35008#236027]

---
+++ chapter_003/src/main/java/collection/Sort/StringCompare.java
@@ -0,0 +1,20 @@
+package collection.Sort;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int rsl = 0;
+        int len1 = left.length();
+        int len2 = right.length();
+        int len = Math.min(len1, len2);
+        for (int i = 0; i < len; i++) {
+            rsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (rsl != 0) {
+                return rsl;
+            }
+        }
+        return Integer.compare(len1, len2);
+    }
+}
