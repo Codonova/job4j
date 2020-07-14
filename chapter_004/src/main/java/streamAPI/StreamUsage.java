db2e0a7ae8de397be254cbd2efbc0b24c5735f0b
Codonova <r.kosolapchenkov@gmail.com>
2020-07-14 13:04:25 +0300

    Stream API[58166#235894]

---
+++ chapter_004/src/main/java/streamAPI/StreamUsage.java
@@ -0,0 +1,23 @@
+package streamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static List<Integer> stream(List<Integer> numbers) {
+        List<Integer> rsl = numbers.stream().filter(
+                num -> num >= 0
+        ).collect(Collectors.toList());
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 5, 2, 9, -3, -5, 4);
+        for (Integer value : stream(numbers)) {
+            System.out.println(value);
+        }
+
+    }
+}
