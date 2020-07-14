58f44bc870e4f623f126a6747680099999711c64
Codonova <r.kosolapchenkov@gmail.com>
2020-07-14 11:49:59 +0300

    В классе StartUI заменить вывод в консоль на Consumer.[24255#235887]

--- chapter_003/src/main/java/collection/list/PhoneDictionary.java
+++ chapter_003/src/main/java/collection/list/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package collection.list;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -10,11 +11,24 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> rsl = new ArrayList<Person>();
-        for (Person data : this.persons) {
+        ArrayList<Person> rsl = new ArrayList<>();
+        Predicate<Person> findByName = (name) -> name.getName().equals(key);
+        Predicate<Person> findBySurname = (surname) -> surname.getSurname().equals(key);
+        Predicate<Person> findByPhone = (phone) -> phone.getPhone().equals(key);
+        Predicate<Person> findByAddress = (address) -> address.getAddress().equals(key);
+        Predicate<Person> combine = findByName.or(findBySurname.or(findByPhone.or(findByAddress)));
+
+       /* for (Person data : this.persons) {
            if (data.getName().contains(key) || data.getSurname().contains(key) || data.getAddress().contains(key) || data.getAddress().contains(key)) {
                rsl.add(data);
            }
+        }
+        */
+
+        for (Person person : this.persons) {
+            if (combine.test(person)) {
+                rsl.add(person);
+            }
         }
         return rsl;
     }
