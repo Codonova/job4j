fb97ad191e6c6413d5408ee67ed048ed0f48c291
Codonova <r.kosolapchenkov@gmail.com>
2020-07-15 13:32:53 +0300

    Список адресов[110062#235896]

---
+++ chapter_004/src/main/java/streamAPI/Profiles.java
@@ -0,0 +1,10 @@
+package streamAPI;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(p -> p.getAddress()).collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/test/java/ProfilesTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import streamAPI.Address;
+import streamAPI.Profile;
+import streamAPI.Profiles;
+
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    @Test
+    public void whenListAddress() {
+        Address address1 = new Address("Moscow", "Luganskaya", 6, 333);
+        Address address2 = new Address("NY", "123", 3, 543);
+        List<Profile> input = List.of(
+                new Profile(address1),
+                new Profile(address2)
+        );
+        List<Address> rsl = Profiles.collect(input);
+        List<Address> expect = List.of(
+                new Address("Moscow", "Luganskaya", 6, 333),
+                new Address("NY", "123", 3, 543)
+        );
+
+    assertThat(rsl, is(expect));
+    }
+}
