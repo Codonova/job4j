661939884576f5008f1fb77946f8a45a822c376e
Codonova <r.kosolapchenkov@gmail.com>
2020-07-16 15:25:27 +0300

    Фильтрация учеников.[110058#235895] Correct

--- chapter_004/src/test/java/SchoolTest.java
+++ chapter_004/src/test/java/SchoolTest.java
@@ -2,25 +2,21 @@ import org.junit.Test;
 import streamAPI.School;
 import streamAPI.Student;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
 public class SchoolTest {
     @Test
     public void whenClassA() {
-        Predicate<Student> minA = s -> s.getScore() >= 70;
-        Predicate<Student> maxA = s -> s.getScore() <= 100;
-        Predicate<Student> combine = minA.and(maxA);
         List<Student> input = List.of(
                 new Student("Kolobkov", 79),
                 new Student("Ivanov", 63),
                 new Student("Abramov", 82),
                 new Student("Kurbatov", 33)
         );
-        List<Student> rsl = School.collect(input, combine);
+        List<Student> rsl = School.collect(input, s -> s.getScore() > 70 && s.getScore() < 100);
         List<Student> expect = List.of(
                 new Student("Kolobkov", 79),
                 new Student("Abramov", 82)
@@ -30,16 +26,13 @@ public class SchoolTest {
 
     @Test
     public void whenClassB() {
-        Predicate<Student> minB = s -> s.getScore() >= 50;
-        Predicate<Student> maxB = s -> s.getScore() <= 70;
-        Predicate<Student> combine = minB.and(maxB);
         List<Student> input = List.of(
                 new Student("Kolobkov", 79),
                 new Student("Ivanov", 63),
                 new Student("Abramov", 82),
                 new Student("Kurbatov", 33)
         );
-        List<Student> rsl = School.collect(input, combine);
+        List<Student> rsl = School.collect(input, s -> s.getScore() > 50 && s.getScore() < 70);
         List<Student> expect = List.of(
                 new Student("Ivanov", 63)
         );
@@ -48,16 +41,13 @@ public class SchoolTest {
 
     @Test
     public void whenClassC() {
-        Predicate<Student> minB = s -> s.getScore() >= 0;
-        Predicate<Student> maxB = s -> s.getScore() <= 50;
-        Predicate<Student> combine = minB.and(maxB);
         List<Student> input = List.of(
                 new Student("Kolobkov", 79),
                 new Student("Ivanov", 63),
                 new Student("Abramov", 82),
                 new Student("Kurbatov", 33)
         );
-        List<Student> rsl = School.collect(input, combine);
+        List<Student> rsl = School.collect(input, s -> s.getScore() > 0 && s.getScore() < 50);
         List<Student> expect = List.of(
                 new Student("Kurbatov", 33)
         );
