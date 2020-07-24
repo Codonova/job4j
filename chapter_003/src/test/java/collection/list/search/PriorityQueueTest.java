91006196714144a7a75456eef4de172224d86230
Codonova <r.kosolapchenkov@gmail.com>
2020-07-24 16:15:09 +0300

    Local-Variable Type Inference[70626#235903]

--- chapter_003/src/test/java/collection/list/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/collection/list/search/PriorityQueueTest.java
@@ -10,11 +10,11 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue pq = new PriorityQueue();
+        var pq = new PriorityQueue();
         pq.put(new Task("low", 5));
         pq.put(new Task("urgent", 1));
         pq.put(new Task("middle", 3));
-        Task result = pq.take();
+        var result = pq.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 }
