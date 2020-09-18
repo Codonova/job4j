96b057cb1f77157d6255675986708ec1ff32b4ca
Codonova <r.kosolapchenkov@gmail.com>
2020-09-18 10:38:47 +0300

    1.2. Режим ожидания.[231217#362251]

---
+++ chapter_005/src/main/java/multithreading/Wget.java
@@ -0,0 +1,19 @@
+package multithreading;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 100; i++) {
+                            System.out.print("\rLoading: " + i + "%");
+                            Thread.sleep(500);
+                        }
+                    } catch (InterruptedException ie) {
+                        ie.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
