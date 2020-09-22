1bb9621ead823fc4a296362a8c63b5b601d97e6a
Codonova <r.kosolapchenkov@gmail.com>
2020-09-22 09:50:47 +0300

    Прерывание нити[1019#362254]

---
+++ chapter_005/src/main/java/multithreading/ConsoleProgress.java
@@ -0,0 +1,38 @@
+package multithreading;
+
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int i = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r load: " + process(i++));
+                Thread.sleep(500);
+                if (i == 3) {
+                    i = 0;
+                }
+            }
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    public static char process(int i) {
+        char c = '\\';
+        if (i == 1) {
+            c = '|';
+        } else if (i == 2) {
+            c = '/';
+        }
+        return c;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+           Thread progress = new Thread(new ConsoleProgress());
+           progress.start();
+           Thread.sleep(3000);
+           progress.interrupt();
+    }
+}
