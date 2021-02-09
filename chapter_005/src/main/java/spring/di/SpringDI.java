ea738791ddc28ba7293f6aa5a2b118445ff075f9
Codonova <r.kosolapchenkov@gmail.com>
2021-02-09 17:03:55 +0300

    Spring DI. Запуск[2091#362212]

---
+++ chapter_005/src/main/java/spring/di/SpringDI.java
@@ -0,0 +1,15 @@
+package spring.di;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.askInt("2");
+        ui.askStr("Как дела?");
+    }
+
+}
