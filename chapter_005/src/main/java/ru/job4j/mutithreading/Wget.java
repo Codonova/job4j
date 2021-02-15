18c1ad0496a7e2388f3a1312fa277101d557e3ce
Codonova <r.kosolapchenkov@gmail.com>
2021-02-15 14:51:22 +0300

    Скачивание файла с ограничением.[144271#362249] (with validate)

--- chapter_005/src/main/java/ru/job4j/mutithreading/Wget.java
+++ chapter_005/src/main/java/ru/job4j/mutithreading/Wget.java
@@ -15,37 +15,51 @@ public class Wget implements Runnable {
         this.speed = speed;
     }
 
+    public boolean validate(String url, int speed) {
+        boolean validate = false;
+        if (url != "" && speed != 0) {
+            validate = true;
+        }
+        return validate;
+    }
+
     @Override
     public void run() {
-
         long startTime;
         long size = 0;
 
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte[] dataBuffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                startTime = System.currentTimeMillis();
-                size += bytesRead;
-                if (size >= speed) {
-                    if ((System.currentTimeMillis() - startTime) < 1000L)
-                        Thread.sleep(1000L - (System.currentTimeMillis() - startTime));
+        if (validate(url, speed)) {
+            try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+                byte[] dataBuffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    startTime = System.currentTimeMillis();
+                    size += bytesRead;
+                    if (size >= speed) {
+                        if ((System.currentTimeMillis() - startTime) < 1000L)
+                            Thread.sleep(1000L - (System.currentTimeMillis() - startTime));
+                    }
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                    size = 0;
                 }
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                size = 0;
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Download success.");
+        }
+        else {
+            System.out.println("Wrong parameters.");
         }
     }
 
 
     public static void main(String[] args) throws InterruptedException {
-        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        int speed = Integer.parseInt("512");
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
     }
 }
+
